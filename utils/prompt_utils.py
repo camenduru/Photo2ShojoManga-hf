2419111a2b7809b734e36eced83fb72db2c1e666
tori29umai <tori29umai@users.noreply.huggingface.co>
2024-11-06 06:52:33 +0000

    Update utils/prompt_utils.py

--- utils/prompt_utils.py
+++ utils/prompt_utils.py
@@ -14,7 +14,7 @@ def remove_duplicates(base_prompt):
 def remove_color(base_prompt):
     # タグの色情報を取り除く
     prompt_list = base_prompt.split(", ")
-    color_list = ["pink", "red", "orange", "brown", "yellow", "green", "blue", "purple", "blonde", "colored skin", "white hair", "gray"]
+    color_list = ["pink", "red", "orange", "brown", "yellow", "green", "blue", "purple", "blonde", "colored skin", "white hair", "gray", "black"]
     # カラータグを除去します。
     cleaned_tags = [tag for tag in prompt_list if all(color.lower() not in tag.lower() for color in color_list)]
     return ", ".join(cleaned_tags)
