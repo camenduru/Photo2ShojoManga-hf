6edd6df8c6538678ef62b754d9fbae1ad015fa71
tori29umai <tori29umai@users.noreply.huggingface.co>
2024-11-09 03:00:03 +0000

    Update utils/dl_utils.py

--- utils/dl_utils.py
+++ utils/dl_utils.py
@@ -58,9 +58,8 @@ def dl_tagger_model(model_dir):
 
 def dl_lora_model(model_dir):
     models = {
-        "syoujomannga_line.safetensors": "https://huggingface.co/tori29umai/Egara_Lora/resolve/main/syoujomannga_line.safetensors",
+        "tcd-animaginexl-3_1.safetensors": "https://huggingface.co/furusu/SD-LoRA/blob/main/tcd-animaginexl-3_1.safetensors",
         "tori29umai_line.safetensors": "https://huggingface.co/tori29umai/Egara_Lora/resolve/main/tori29umai_line.safetensors",
-    # "gekiga_line.safetensors": "https://huggingface.co/tori29umai/Egara_Lora/resolve/main/gekiga_line.safetensors"
     }
     
     # 各モデルを確認し、必要に応じてダウンロード
